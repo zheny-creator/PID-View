3564aecee75494d34405d2fbdbd17ed0d5415da2
zheny-creator <rimkamix0@gmail.com>
2026-06-16 23:36:00 +0300

    add watch mode for real-time process monitoring

--- src/PID_view.c
+++ src/PID_view.c
@@ -9,6 +9,7 @@
 #include <pwd.h>
 #include <time.h>
 #include <getopt.h>
+#include <stdbool.h>
 
 struct process_in_ram
 {
@@ -190,6 +191,7 @@ void usage(void)
 
 int main(int argv, char *args[])
 {
+    int watch = 0;
     if (argv < 2)
     {
         usage();
@@ -213,7 +215,7 @@ int main(int argv, char *args[])
             return 0;
             break;
         case 'w':
-            printf("Coming soon!");
+            watch = 1;
             break;
         default:
             fprintf(stderr, "Unknown argument!");
@@ -221,52 +223,81 @@ int main(int argv, char *args[])
         }
     }
     struct process_in_ram process = {0};
-    process.pid = atoi(args[1]);
-    char filepath[32];
-    char buffer[1024];
-
-    snprintf(filepath, sizeof(filepath), "/proc/%d/status", process.pid);
-
-    int fd = open(filepath, O_RDONLY);
-    if (fd < 0)
+    if (watch == 0)
     {
-        fprintf(stderr, "Process %d not found\n", process.pid);
-        return 1;
+        process.pid = atoi(args[1]);
     }
-
-    ssize_t bytes_read = read(fd, buffer, sizeof(buffer) - 1);
-    close(fd);
-
-    if (bytes_read <= 0)
+    else
     {
-        fprintf(stderr, "Failed to read status\n");
-        return 1;
+        process.pid = atoi(args[2]);
     }
-    buffer[bytes_read] = '\0';
-
-    parse_proc_status(buffer, &process);
-    parse_advanced_metrics(&process);
-    parse_all_aviable_memory(&process);
-
-    long h = process.uptime_sec / 3600;
-    long m = (process.uptime_sec % 3600) / 60;
-    long s = process.uptime_sec % 60;
-
-    printf("=== PID-View v0.4 ===\n");
-    printf("PID:          %d\n", process.pid);
-    printf("Name:         %s\n", process.name);
-    printf("Owner:        %s\n", process.uid_name);
-    printf("State:        %c\n", process.state);
-    printf("Priority:     %d\n", process.priority - 20);
-    if (process.ppid != 0)
+    while (true)
     {
-        printf("Parent PID:   %d\n", process.ppid);
+        char filepath[32];
+        char buffer[1024] = {0};
+
+        snprintf(filepath, sizeof(filepath), "/proc/%d/status", process.pid);
+
+        int fd = open(filepath, O_RDONLY);
+        if (fd < 0)
+        {
+            fprintf(stderr, "Process %d not found\n", process.pid);
+            return 1;
+        }
+
+        ssize_t bytes_read = read(fd, buffer, sizeof(buffer) - 1);
+        close(fd);
+
+        if (bytes_read <= 0)
+        {
+            fprintf(stderr, "Failed to read status\n");
+            return 1;
+        }
+        buffer[bytes_read] = '\0';
+
+        parse_proc_status(buffer, &process);
+        parse_advanced_metrics(&process);
+        parse_all_aviable_memory(&process);
+
+        long h = process.uptime_sec / 3600;
+        long m = (process.uptime_sec % 3600) / 60;
+        long s = process.uptime_sec % 60;
+
+        printf("=== PID-View v0.4 ===\n");
+        printf("PID:          %d\033[K\n", process.pid);
+        printf("Name:         %s\033[K\n", process.name);
+        printf("Owner:        %s\033[K\n", process.uid_name);
+        printf("State:        %c\033[K\n", process.state);
+        printf("Priority:     %d\033[K\n", process.priority - 20);
+
+        if (process.ppid != 0)
+        {
+            printf("Parent PID:   %d\033[K\n", process.ppid);
+        }
+        else
+        {
+            printf("Parent PID:   None\033[K\n");
+        }
+
+        printf("Threads:      %d\033[K\n", process.threads);
+        printf("Virtual Mem:  %ld MB\033[K\n", process.vmem_mb);
+        printf("Physical Mem: %ld MB (RSS) (%.2f%%)\033[K\n", process.rss_mb, process.all_memory);
+        printf("Running Time: %02ld:%02ld:%02ld\033[K\n", h, m, s);
+        printf("=====================\n");
+
+        fflush(stdout);
+
+        sleep(1);
+        if (watch == 0)
+        {
+            break;
+        }
+        else
+        {
+            printf("\033[12A");
+            continue;
+        }
     }
-    printf("Threads:      %d\n", process.threads);
-    printf("Virtual Mem:  %ld MB\n", process.vmem_mb);
-    printf("Physical Mem: %ld MB (RSS) (%.2f%%)\n", process.rss_mb, process.all_memory);
-    printf("Running Time: %02ld:%02ld:%02ld\n", h, m, s);
-    printf("=====================\n");
 
     return 0;
 }
